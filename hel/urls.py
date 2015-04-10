93eb33ecd8c6fbfc31833fb53248556b8810c497
Christian Meter <cmeter@googlemail.com>
2015-04-10 17:47:51 +0200

    Well... fuck you templates -.-

--- hel/urls.py
+++ hel/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'hel.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
