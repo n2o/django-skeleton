cd8f5cb3a370e9daf679ff05b92e79af07abec6f
Christian Meter <cmeter@googlemail.com>
2015-04-10 17:08:58 +0200

    Thats part 3

--- hel/urls.py
+++ hel/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'hel.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
