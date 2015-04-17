2a66ea55023027e48ed16745b8f10c5e4d79c2de
Christian Meter <cmeter@googlemail.com>
2015-04-17 17:30:17 +0200

    Add polls migrations

---
+++ polls/migrations/0002_auto_20150417_1423.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='question',
+            name='pub_date',
+            field=models.DateTimeField(verbose_name='date published'),
+        ),
+    ]
