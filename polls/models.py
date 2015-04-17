4dc0fce4fefdf0302bae16243a8ee48bb571b0ed
Christian Meter <cmeter@googlemail.com>
2015-04-17 15:38:11 +0200

    Switch to python3

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def __unicode__(self):
+    def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
@@ -17,5 +17,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.choice_text
