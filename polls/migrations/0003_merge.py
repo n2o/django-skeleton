0f34110ef1705244bd55bc0296792fba362e5d2b
Christian Meter <cmeter@googlemail.com>
2015-04-20 21:44:40 +0200

    Update migrations

---
+++ polls/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0002_auto_20150417_1345'),
+        ('polls', '0002_auto_20150417_1423'),
+    ]
+
+    operations = [
+    ]
