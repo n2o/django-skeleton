670a6227d5138ef23346780ff45b0985eb802f6b
Christian Meter <cmeter@googlemail.com>
2015-04-20 22:02:16 +0200

    Set URLs for login

--- hel/urls.py
+++ hel/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^pages/', include('django.contrib.flatpages.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^polls/', include('login.urls', namespace='login')),
 ]
---
+++ login/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from login.views import *
+
+urlpatterns = patterns('',
+    url(r'^$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', logout_page),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
+    url(r'^register/$', register),
+    url(r'^register/success/$', register_success),
+    url(r'^home/$', home),
+)
