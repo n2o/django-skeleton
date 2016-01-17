2b1c1f03c01a8bcd2d52b72ac66eb2528cc74d03
n2o <christian.meter@uni-duesseldorf.de>
2016-01-17 15:16:08 +0100

    Use named urls in login app

--- login/urls.py
+++ login/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from login.views import *
 
 
 urlpatterns = patterns('',
     url(r'^$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', logout_page),
-    url(r'^login/$', 'django.contrib.auth.views.login'),  # If user is not logged in, it will redirect to login page
-    url(r'^register/$', register),
-    url(r'^register/success/$', register_success),
+    url(r'^logout/$', logout_page, name='logout'),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),  # If user is not logged in, it will redirect to login page
+    url(r'^register/$', register, name='register'),
+    url(r'^register/success/$', register_success, name='register_success'),
 )
