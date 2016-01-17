f89d9b0557f5b2b9f94f2113482598f57da1ba2e
n2o <christian.meter@uni-duesseldorf.de>
2016-01-17 15:11:37 +0100

    Update urls and views

--- login/urls.py
+++ login/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
 from login.views import *
 
-import login.urls
 
 urlpatterns = patterns('',
     url(r'^$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', logout_page),
-    url(r'^login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
+    url(r'^login/$', 'django.contrib.auth.views.login'),  # If user is not logged in, it will redirect to login page
     url(r'^register/$', register),
     url(r'^register/success/$', register_success),
 )
--- login/views.py
+++ login/views.py
@@ -13,9 +13,9 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             user = User.objects.create_user(
-                username = form.cleaned_data['username'],
-                password = form.cleaned_data['password1'],
-                email = form.cleaned_data['email']
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+                email=form.cleaned_data['email']
             )
             return HttpResponseRedirect('/account/register/success/')
     else:
@@ -42,5 +42,5 @@ def logout_page(request):
 def home(request):
     return render_to_response(
         'index.html',
-        { 'user': request.user }
+        {'user': request.user}
     )
