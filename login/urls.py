af49b5e052f0ab33bf5c39c60807cecabb2a99ed
n2o <christian.meter@uni-duesseldorf.de>
2016-01-25 12:55:30 +0100

    Add compatibiliy to Django 1.10

--- login/urls.py
+++ login/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url
 from login.views import *
 
+import django.contrib.auth.views as authviews
+
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.contrib.auth.views.login'),
+    url(r'^$', authviews.login),
     url(r'^logout/$', logout_page, name='logout'),
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),  # If user is not logged in, it will redirect to login page
+    url(r'^login/$', authviews.login, name='login'),  # If user is not logged in, it will redirect to login page
     url(r'^register/$', register, name='register'),
     url(r'^register/success/$', register_success, name='register_success'),
 )
